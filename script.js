f6c91da23f6b9ea0b089ce898e5a6a9fbd5736bb
legodud3 <110813772+legodud3@users.noreply.github.com>
2024-01-26 16:52:17 +0530

    asdas

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ window.onload = function() {
             console.log("CSV Data Loaded: ", result.data);
             // Date of the first row's record
             console.log("First row, first column data: ", result.data[0].time);
+            // Display this 'time' data in the 'output' paragraph in the HTML
+            document.getElementById("output").innerText = result.data[0].time;
         },
         error: function(err, file) {
             console.log("ERROR:", err, file);
