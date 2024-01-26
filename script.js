20294354d21563875ff69165aceab083cb043603
legodud3 <110813772+legodud3@users.noreply.github.com>
2024-01-26 16:48:16 +0530

    display data in first column of csv

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 window.onload = function() {
-    // Parse local CSV file
+    // Parse CSV file
     Papa.parse('US_City_Temp_Data.csv', {
         download: true,
         header: true,
         complete: function(result, file) {
             console.log("CSV Data Loaded: ", result.data);
+            // Date of the first row's record
+            console.log("First row, first column data: ", result.data[0].time);
         },
         error: function(err, file) {
             console.log("ERROR:", err, file);
         }
     });
 };
-
-window.loadTemperature() =function() {
-    let city = "atlanta";
-    let date = "01/04/1984";
-    
-    let matchingData = window.dataset.find(function(row) {
-        return row["time"].toLowerCase() === date.toLowerCase() && row[city];
-    });
-    
-    if (matchingData) {
-        document.getElementById("temperature").innerText = `Temperature in ${city} on ${date}: ${matchingData[city]}`;
-    } else {
-        document.getElementById("temperature").innerText = `No data found for ${city} on ${date}.`;
-    }
-}
