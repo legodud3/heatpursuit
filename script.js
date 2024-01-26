12e0a2d34ad90f5c10bef63ed535083377bcc34a
legodud3 <110813772+legodud3@users.noreply.github.com>
2024-01-26 17:00:29 +0530

    fetch temp on 01/01/48 of a user entered city

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-window.onload = function() {
-    // Parse CSV file
-    Papa.parse('US_City_Temp_Data.csv', {
-        download: true,
-        header: true,
-        complete: function(result, file) {
-            console.log("CSV Data Loaded: ", result.data);
-            // Date of the first row's record
-            console.log("First row, first column data: ", result.data[0].time);
-            // Display this 'time' data in the 'output' paragraph in the HTML
-            document.getElementById("output").innerText = result.data[0].time;
-        },
-        error: function(err, file) {
-            console.log("ERROR:", err, file);
-        }
-    });
-};
+// Parse CSV file
+Papa.parse('US_City_Temp_Data.csv', {
+    download: true,
+    header: true,
+    complete: function(result, file) {
+        console.log("CSV Data Loaded: ", result.data);
+        window.data = result.data;
+    },
+    error: function(err, file) {
+        console.log("ERROR:", err, file);
+    }
+});
+
+function searchCity() {
+    var cityName = document.getElementById('city').value.trim();  // trim() is used to remove white spaces at the beginning and the end
+    if (window.data[0][cityName]) {  // If city found in the first row of CSV
+        document.getElementById('output').innerText = `City found! Temperature on 01/01/48: ${window.data[0][cityName]}`;
+    } else {
+        document.getElementById('output').innerText = 'City not found.';
+    }
+}
